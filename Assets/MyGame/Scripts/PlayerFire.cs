82763c6a78de39be6372d96133c633fac5a13060
langtu262 <trungkienit87@gmail.com>
2024-04-10 16:03:00 +0700

    1

---
+++ Assets/MyGame/Scripts/PlayerFire.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public Transform pointFire;
+    public Rigidbody2D rb;
+    public float projectiveFore = 20f;
+    public GameObject projective;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetButtonDown("Fire1"))
+        {
+
+        }
+    }
+}
