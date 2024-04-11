30b8dd95497f21e98491284e672d46422eef6ed8
langtu262 <trungkienit87@gmail.com>
2024-04-11 21:10:18 +0700

    1

--- Assets/MyGame/Scripts/Projective.cs
+++ Assets/MyGame/Scripts/Projective.cs
@@ -6,6 +6,7 @@ public class Projective : MonoBehaviour
 {
     public int damage = 5;
     public Vector2 fore;
+    [SerializeField] GameObject Fxprojective;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision != null && collision.CompareTag("Enemy"))
@@ -14,6 +15,7 @@ public class Projective : MonoBehaviour
             BehemothAI behemothAI = collision.GetComponent<BehemothAI>();
             if (behemothAI != null)
             {
+                Instantiate(Fxprojective,transform.position, Quaternion.identity);
                 behemothAI.TakeDamage(damage, fore, gameObject);
                 Destroy(gameObject);
             }
